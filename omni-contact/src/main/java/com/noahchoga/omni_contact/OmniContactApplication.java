7f7bc57c3ff171ab8bc2be9bb6adf197a54cb97b
chogan1108 <choganoah@gmail.com>
2026-09-09 11:18:30 +0200

    Initial commit

---
+++ omni-contact/src/main/java/com/noahchoga/omni_contact/OmniContactApplication.java
@@ -0,0 +1,13 @@
+package com.noahchoga.omni_contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniContactApplication.class, args);
+	}
+
+}
---
+++ omni-contact/src/test/java/com/noahchoga/omni_contact/OmniContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noahchoga.omni_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
